55146c9bd28779c84ad1b7dafcddea728cab59de
Alevtyna <alevtynafrantsuz@gmail.com>
2026-09-19 13:36:57 +0300

    Create lab1(64).py

---
+++ lab1(64).py
@@ -0,0 +1,11 @@
+a1 = float(input())
+a2 = float(input())
+a3 = float(input())
+
+result1 = a1 - int(a1)
+result2 = a2 - int(a2)
+result3 = a3 - int(a3)
+
+print(round(result1, 3))
+print(round(result2, 3))
+print(round(result3, 3))
