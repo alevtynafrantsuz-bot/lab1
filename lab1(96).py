03e6e72f72d7e781fa21255339c9bfda59499488
Alevtyna <alevtynafrantsuz@gmail.com>
2026-09-19 13:52:44 +0300

    Create lab1(96).py

---
+++ lab1(96).py
@@ -0,0 +1,11 @@
+a1 = float(input())
+a2 = float(input())
+a3 = float(input())
+
+result1 = (a1 % 30) * 12
+result2 = (a2 % 30) * 12
+result3 = (a3 % 30) * 12
+
+print(result1)
+print(result2)
+print(result3)
