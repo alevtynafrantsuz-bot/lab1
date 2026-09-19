e7f79440d30a8249a0a4bb0bfa214b13f7de5057
Alevtyna <alevtynafrantsuz@gmail.com>
2026-09-19 13:31:33 +0300

    Create lab1(43).py

---
+++ lab1(43).py
@@ -0,0 +1,7 @@
+a = input()
+b = int(input())
+c = float(input())
+
+print(f'Value of a is "{a}"? and it`s type {type(a).__name__}.')
+print(f'Value of a is "{b}"? and it`s type {type(b).__name__}.')
+print(f'Value of a is "{c}"? and it`s type {type(c).__name__}.')
